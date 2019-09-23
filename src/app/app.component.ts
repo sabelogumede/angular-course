35855b2c6acbfc10ffa562b511da9ed20a9de4db
sabelogumede <iamsabelogumede@gmail.com>
2019-09-23 11:27:14 +0200

    ng built-in pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,11 @@ export class AppComponent {
 
   courses = COURSES;
 
+  // title = COURSES[0].description;
+  // price = 9.922330985;
+  // startDate = new Date(2000, 0, 1);
+  // rate = 0.67;
+
   onCourseSelected(course: Course) {
     console.log('App component - click event bubbled...', course);
   }
